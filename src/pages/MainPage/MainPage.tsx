1da461c51588e8ef6d93c26ad216e53a0e199d6c
Kirill <kirillbar24@gmail.com>
2024-09-04 18:04:47 +0400

    fix

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,10 +2,12 @@ import { MainBanner, NewCollectionBanner } from '@/widgets';
 
 import styles from './MainPage.module.scss';
 import { QuestionBanner } from '@/widgets/Banners';
+import { CategoryCard } from '@/entities/category';
 
 const MainPage = () => {
     return (
         <div className={styles.mainPage}>
+            <CategoryCard />
             <MainBanner className={styles.mainBanner} />
             <NewCollectionBanner className={styles.newCollectionBanner} />
             <QuestionBanner />
